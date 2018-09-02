faa5c767196a220cc35aeb2d65d0665a143271f2
Kopranych <kopranych@gmail.com>
2018-09-02 18:40:54 +0300

    [REFACTOR] - YandexTranslateTest

--- src/test/java/YandexTranslateTest.java
+++ src/test/java/YandexTranslateTest.java
@@ -3,11 +3,13 @@ import api.yandex.translate.impl.YandexTranslateImpl;
 import org.junit.Assert;
 import org.junit.Test;
 
+
 public class YandexTranslateTest {
     @Test
-    public void testTranslate(){
+    public void testTranslate() {
         YandexTranslate yandexTranslate = new YandexTranslateImpl();
-        String text = yandexTranslate.translateEnRu("api yandex translate is a good service");
-        Assert.assertEquals("API Яндекс перевод хороший сервис", text);
+        String text = "api yandex translate is a good service";
+        String translate = yandexTranslate.translateEnRu(text);
+        Assert.assertEquals("API Яндекс перевод хороший сервис", translate);
     }
 }
